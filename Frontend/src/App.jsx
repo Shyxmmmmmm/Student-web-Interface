de998aa5c493f62d225e5b26309c9d5b79e48f9a
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-05-02 22:55:05 +0530

    fix case issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {Route,Routes } from 'react-router-dom'
 import Frontpage from './Components/Frontpage'
 import Addstudent from './Components/Addstudent'
 import Viewstudent from './Components/Viewstudent'
-import Editstudent from './Components/EditStudent'
+import Editstudent from './Components/Editstudent'
 import Signup from './Components/Signup'
 import Login from './Components/Login'
 const App=()=>{
