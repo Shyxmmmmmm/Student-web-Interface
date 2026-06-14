890378305aca27a6b7156c99e4e9ddac73b04fc5
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-06-14 09:34:38 +0530

    data inserted

--- Frontend/API_URL.js
+++ Frontend/API_URL.js
@@ -2,6 +2,6 @@
 const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "https://student-web-interface-2.onrender.com/"
+    : "https://student-web-interface-2.onrender.com"
 
 export default API_URL
